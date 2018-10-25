e60dac9483a89097e878507170bc5f8c29d8cfd7
rudywijaya <rudy_sukki@yahoo.co.id>
2018-10-25 11:37:38 +0700

    commit another new edited file

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // })
+
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // })
+
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // })
+
+  db.collection('Users').deleteOne({
+    _id: 123
+  }).then((result) => {
+    console.log(result);
+  })
+
+  // client.close();
+});
