57b1753c1aa8a73e3118d63d5fa16e9df1d77fb7
rudywijaya <rudy_sukki@yahoo.co.id>
2018-11-12 15:32:36 +0700

    Update expect version to the latest

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -183,6 +183,7 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBeTruthy();
         // expect(res.body.todo.completedAt).toBe('number');
+        expect(typeof res.body.todo.completedAt).toBe('number');
       })
       .end(done);
   });
@@ -314,7 +315,7 @@ describe('POST /users/login', () => {
         }
 
         User.findById(users[1]._id).then((user) => {
-          expect(user.tokens[1]).toMatchObject({
+          expect(user.toObject().tokens[1]).toMatchObject({
             access: 'auth',
             token: res.headers['x-auth']
           });
